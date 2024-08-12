ab87538632871badffcbe1166dff6803fab7d61a
Sypth <105406568+Sypth@users.noreply.github.com>
2024-08-12 15:45:29 +0800

    :wrench: Changing title, adding, and deleting options dynamically

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -5,5 +5,24 @@
         <input type="text" wire:model.live="title" />
 
         Current title: {{ $title }}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div class="mt-4">
+            @foreach($options as $index => $option)
+                <div class="mt-4">
+                    <label>Option {{ $index + 1 }}</label>
+                    <div class="flex gap">
+                        <input type="text" wire:model.live="options.{{ $index }}"/>
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+
+
+            @endforeach
+        </div>
+
     </form>
 </div>
